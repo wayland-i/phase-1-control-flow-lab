357022698f95402355e61519ac217a85d321ad5e
Ian Iversen <ianiversen1@gmail.com>
2022-06-28 17:11:30 -0400

    commit tree cf1c05582038503321bae5b0fa3b2a2753ab590a

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride) {
+  if (ride <= 400) {
+    return "This one is on me!";
+  } else if (ride >= 400 && ride <= 2000) {
+      return 'That will be twenty bucks.';
+  } else if (ride > 2000 && ride <= 2500) {
+      return 'I will gladly take your thirty bucks.';
+  } else if (ride > 2500) {
+      return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+};
+
+
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+ 
